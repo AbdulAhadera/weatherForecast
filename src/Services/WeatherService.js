bd2be61f27ab9f768d85d90af49690c9af58ad26
AbdulAhadera <abdulahad.era@gmail.com>
2023-11-30 19:45:19 -0600

    last and end due to weather api

--- src/Services/WeatherService.js
+++ src/Services/WeatherService.js
@@ -36,21 +36,21 @@ const formatForecastWeather = (data) => {
     let { timezone, daily, hourly } = data;
 
 
-    daily = daily.slice(1, 6).map(d => {
-        return {
-            title: formatToLocalTime(d.dt, timezone, 'ccc'),
-            temp: d.temp.day,
-            icon: d.weather[0].icon
-        }
-    });
+    // daily = daily.slice(1, 6).map(d => {
+    //     return {
+    //         title: formatToLocalTime(d.dt, timezone, 'ccc'),
+    //         temp: d.temp.day,
+    //         icon: d.weather[0].icon
+    //     }
+    // });
 
-    hourly = hourly.slice(1, 6).map(d => {
-        return {
-            title: formatToLocalTime(d.dt, timezone, 'ccc'),
-            temp: d.temp.day,
-            icon: d.weather[0].icon
-        }
-    });
+    // hourly = hourly.slice(1, 6).map(d => {
+    //     return {
+    //         title: formatToLocalTime(d.dt, timezone, 'ccc'),
+    //         temp: d.temp.day,
+    //         icon: d.weather[0].icon
+    //     }
+    // });
 
 
 
